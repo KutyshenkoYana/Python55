462a931ef1a2969085cd4ca0a373d327eefb0ba2
KutyshenkoYana <jkutishenko1@gmail.com>
2026-05-21 09:37:25 +0200

    FastApi

--- MAINMAIN.py
+++ MAINMAIN.py
@@ -4,6 +4,7 @@
 
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -23,3 +24,51 @@ def register_user(username: str):
         "user": username,
         "is registered": True,
     }
+
+
+# створеня схеми для данних
+
+# дани користувача
+
+
+class User(BaseModel):
+    name: str
+    age: int
+    email: str
+
+
+# @app.post("/register")
+# def register(user: User):
+#     print(user)
+#
+#     return {
+#         "user_name": user.name,
+#         "user_age": user.age,
+#         "user_email": user.email,
+#         "is_registered": True,
+#     }
+
+
+class UserResponse(BaseModel):
+    user_name: str
+    user_age: int
+    user_email: str
+    is_registered: bool
+
+
+@app.post("/register")
+def register(user: User) -> UserResponse:
+    """
+    Register user
+    :param user:
+    :return:
+    """
+    return UserResponse(
+        user_name=user.name,
+        user_age=user.age,
+        user_email=user.email,
+        is_registered=True,
+    )
+
+
+# put, delete, post, get
