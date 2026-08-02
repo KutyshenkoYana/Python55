e84852f8f8869bfa12e76705f495cc1826c0f2b8
KutyshenkoYana <jkutishenko1@gmail.com>
2026-08-02 09:55:32 +0200

    Practical Work

--- Use_SQL_Python/PracticalWork1/main.py
+++ Use_SQL_Python/PracticalWork1/main.py
@@ -32,13 +32,89 @@ def show_doctors_specializations(session):
     FROM DoctorsSpecializations DS
         JOIN DOCTORS D ON DS.DOCTOR_ID = D.ID
         JOIN SPECIALIZATION S ON DS.SPECIALIZATION_ID = S.ID
-"""
+    """
     query = text(query)
 
+    result = session.execute(query)
+    print("doctors surname")
+
+    for row in result:
+        print(row)
+
+
+# show_doctors_specializations(session)
+
+
+def show_doctors_salary(session):
+    query = """
+    SELECT D.SURNAME, D.SALARY + D.PREMIUM
+    FROM DOCTORS D JOIN VACATIONS V ON D.ID = DOCTOR_ID
+    WHERE V.ENDDATE > CURRENT_DATE AND V.START_DATE < CURRENT_DATE
+    """
+
+    query = text(query)
+
+    result = session.execute(query)
+    print("Doctors surname and salary")
+
+    for row in result:
+        print(row)
+
+
+# show_doctors_salary(session)
+
+
+def _show_name_departmens(session):
+    query = """
+    SELECT NAME
+    FROM DEPARTMENTS
+    """
+
+    query = text(query)
     result = session.execute(query)
 
     for row in result:
         print(row)
 
 
-show_doctors_specializations(session)
+def show_wards(session):
+    _show_name_departmens(session)
+    department_name = input("Enter department name: ")
+
+    query = f"""
+    SELECT *
+    FROM WARDS W JOIN DEPARTMENTS D ON D.ID = W.DEPARTMENT_ID
+    WHERE D.NAME = '{department_name}'
+    """
+
+    query = text(query)
+    result = session.execute(query)
+
+    for row in result:
+        print(row)
+
+
+# show_wards(session)
+
+
+def show_donation(session):
+    month_number = input("Enter month (number): ")
+    year_number = input("Enter year: ")
+
+    query = f"""
+    SELECT *
+    FROM DONATIONS D
+        JOIN DEPARTMENT DEP ON D.DEPARTMENT_ID = DEP.ID
+        JOIN SPONSORS S ON D.SPONSOR_ID = S.ID
+    WHERE EXTRACT(MONTH FROM DONATION_DATE) = '{month_number}'
+        AND EXTRACT(YEAR FROM DONATION_DATE) = '{year_number}'
+    """
+
+    query = text(query)
+    result = session.execute(query)
+
+    for row in result:
+        print(row)
+
+
+show_donation(session)
